2e25ad64535db87521b5cd7e243dc7ca0fe16d1f
Sambalicious <stemitope370@gmail.com>
2020-03-26 21:01:24 +0100

    more updates added

---
+++ app.js
@@ -0,0 +1,19 @@
+var name = 'samuel';
+var courses = ['HTML','CSS','Javascript', 'NodeJs' ]
+
+alert(`hello ${name}`)
+console.log(i);
+
+courses.map(course => alert(course));
+let key = courses.length
+
+if(key % 2 === 0){
+
+for (var i=1; i <= 200 ;i++ ){
+    if (i % 2 === 0){
+        alert(i, 'is even')
+        console.log(i)
+    }
+   
+}
+}
